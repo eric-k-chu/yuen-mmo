794c97920aa34dde4ffa7d1ef5007a45017e0951
Eric Chu <Kiyuen88ec@gmail.com>
2025-04-27 12:18:27 -0700

    client

---
+++ src/main/java/com/yuen/mmo/YuenMMOClient.java
@@ -0,0 +1,11 @@
+package com.yuen.mmo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YuenMMOClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
